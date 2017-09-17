8658b65828e76b921c7280c215bb0f8e9518957b
Yasir <yasirch7722@gmail.com>
2017-09-17 21:54:51 +0500

    Added partial forms and destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,16 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def index    
+    @article = Article.all    
+  end
+  
   def create
      @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was sucessfully created"
-       redirect_to article_path(@article)
+       redirect_to articles_path(@article)
       
     else
       render :new
@@ -15,6 +20,30 @@ class ArticlesController < ApplicationController
 
   def show
    @article = Article.find(params[:id])
+  end
+
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = "Article was sucessfully deleted"
+    redirect_to articles_path
+
+  end 
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully created"
+       redirect_to article_path(@article)
+      
+    else
+      render :edit
+    end
   end
   
   private

018fcaa2c0a7a102c8e1bd790adfe12f6e8bd750
Yasir <yasirch7722@gmail.com>
2017-10-10 22:10:17 +0500

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'articles#home'
   get 'about', to: 'articles#about'
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end

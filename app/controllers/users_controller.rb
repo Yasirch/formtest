ccaee7b71e93898298dbe297b4f1534770b0d82c
Yasir <yasirch7722@gmail.com>
2017-10-17 22:25:24 +0500

    Complete Admin with ability to delete the Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   def new
     @user = User.new
   end
@@ -8,8 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] ="Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -36,6 +38,13 @@ class UsersController < ApplicationController
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "Users and all articles created by user has been deleted"
+    redirect_to users_path
+  end
   
   
   private
@@ -48,10 +57,18 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
   end
-      
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin user can perform this action"
+      redirect_to root_path
+    end
+  end
+  
 end
+
